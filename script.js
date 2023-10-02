1eb2e8691a94873bb3e4ef448b37b1edc3c63756
Aknolagon <kalki.prasanna.co@gmail.com>
2023-10-02 10:36:46 +0200

    update css

--- script.js
+++ script.js
@@ -76,8 +76,8 @@ selectCheckbox.forEach((Checkbox) =>
 );
 
 // loading
-const loader = document.querySelector('.loader');
+//const loader = document.querySelector('.loader');
 
-window.addEventListener('load', () => {
-    loader.classList.add('fondu-out');
-})
+//window.addEventListener('load', () => {
+//    loader.classList.add('fondu-out');
+// })
